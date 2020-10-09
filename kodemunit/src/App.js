d22ec90e1ab677127931a9799231bb31ba39720d
Benedict <bmsteve96@gmail.com>
2020-10-09 13:06:25 +0300

    added required dependencies

--- kodemunit/src/App.js
+++ kodemunit/src/App.js
@@ -1,9 +1,14 @@
 import React from "react"
+import { Switch, Route } from "react-router-dom"
 
 function App() {
   return (
     <div className="App">
       <h1>Hello Kodemunit</h1>
+      <Switch>
+        {/* <Route exact path="/" component={Home} />
+        <Route exact path="/about" component={About} /> */}
+      </Switch>
     </div>
   )
 }
