3929a86863e3858d5424144e9c472a62b4fb66f1
Benedict <bmsteve96@gmail.com>
2020-10-23 00:14:17 +0300

    added swiper in project section

--- kodemunit/src/components/home/Projects.js
+++ kodemunit/src/components/home/Projects.js
@@ -1,5 +1,7 @@
 import React from "react"
 import { Link } from "react-router-dom"
+import { Swiper, SwiperSlide } from "swiper/react"
+import "swiper/swiper.scss"
 
 // data
 import projects from "../../data/projects"
@@ -12,17 +14,31 @@ const Projects = () => {
           <h2>Featured Projects</h2>
         </header>
         <div className="showcase">
+        <Swiper
+            spaceBetween={20}
+            // breakpoints={{
+            //   768: {
+            //     width: 768,
+            //     slidesPerView: 1,
+            //   },
+            //   1200: {
+            //     width: 1200,
+            //     slidesPerView: 3,
+            //   },
+            // }}
+          >
           {projects.map(
             ({ name, slug, url, imgSrc, category, product }, index) => (
+              <SwiperSlide key={index}>
               <article key={index}>
                 <div className="top-details">
                   <h3>{name}</h3>
                   {category && <p>#{category[0]}</p>}
                 </div>
                 <picture>
-                  <source media="(min-width:650px)" srcSet="" />
-                  <source media="(min-width:465px)" srcSet="" />
-                  <img src="" alt="" />
+                  {/* <source media="(min-width:650px)" srcSet={imgSrc} />
+                  <source media="(min-width:465px)" srcSet="" /> */}
+                  <img src={imgSrc} alt={name} />
                 </picture>
                 <div className="project details">
                   {product ? (
@@ -46,8 +62,10 @@ const Projects = () => {
                   )}
                 </div>
               </article>
+              </SwiperSlide>
             )
           )}
+          </Swiper>
         </div>
         <div className="arrows"></div>
       </div>
